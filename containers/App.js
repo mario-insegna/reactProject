9762dd55f3a3e0460e7bc72ad93bd473d85f5f59
Mario Insegna <marioi@alexanders.com>
2018-03-26 22:39:56 -0300

    Setting up a React project from scratch.

---
+++ containers/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class App extends Component {
+    render () {
+        return (
+            <div>
+                <p>This is my new react app from:</p>
+                <a target="_blank" href="https://codeburst.io/setting-up-a-react-project-from-scratch-d62f38ab6d97">https://codeburst.io/setting-up-a-react-project-from-scratch-d62f38ab6d97</a>
+            </div>
+        );
+    }
+}
